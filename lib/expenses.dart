1bf12e1e83c23045deb62fc38612cbe0cb275757
Blue89Azul <life.89.blue@gmail.com>
2023-12-03 07:28:57 +0900

    modified indents.

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -4,18 +4,15 @@ class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
-  State<Expenses> createState () => _ExpenseState();
+  State<Expenses> createState() => _ExpenseState();
 }
 
 class _ExpenseState extends State<Expenses> {
-@override
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
-        children: [
-          Text('There is a chart'),
-          Text('expenses list')
-        ],
+        children: [Text('There is a chart'), Text('expenses list')],
       ),
     );
   }
