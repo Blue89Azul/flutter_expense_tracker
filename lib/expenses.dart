588ae5c3686541e52d827203fe39d6f316bb68d7
Blue89Azul <life.89.blue@gmail.com>
2023-12-02 17:38:54 +0900

    added main frame widget called "expenses".

---
+++ lib/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState () => _ExpenseState();
+}
+
+class _ExpenseState extends State<Expenses> {
+@override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
