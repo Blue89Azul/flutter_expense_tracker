fe697bf9b5911a88bc9913880874b23c8343debc
Blue89Azul <life.89.blue@gmail.com>
2023-12-02 20:47:10 +0900

    added expense data model

---
+++ lib/models/expense.dart
@@ -0,0 +1,19 @@
+import 'package:uuid/uuid.dart';
+
+enum Category { food, travel, leisure, work }
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category
+  }) : id = const Uuid().v4(); // It's ok if it's out of this class.
+  // I suppose that it's a better performance that that class is defined outside...
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+}
